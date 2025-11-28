97759f604ad75ed9d8b95b6a397e03300cf68f3d
riounjanegalura-pixel <riounjanegalura@gmail.com>
2025-11-28 18:54:12 +0800

    UI

--- 07LabExer_Event/FrmClubRegistration.cs
+++ 07LabExer_Event/FrmClubRegistration.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace _07LabExer_Event
 {
-    public partial class Form1 : Form
+    public partial class FrmClubRegistration : Form
     {
-        public Form1()
+        public FrmClubRegistration()
         {
             InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+          
         }
     }
 }
--- 07LabExer_Event/Program.cs
+++ 07LabExer_Event/Program.cs
@@ -16,7 +16,7 @@ namespace _07LabExer_Event
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmClubRegistration());
         }
     }
 }
