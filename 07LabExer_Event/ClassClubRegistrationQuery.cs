476ae7e97901e2e91bda581e315f31a3dec3b9b7
riounjanegalura-pixel <riounjanegalura@gmail.com>
2025-11-28 19:41:29 +0800

    DisplayList method that returns boolean

--- 07LabExer_Event/ClassClubRegistrationQuery.cs
+++ 07LabExer_Event/ClassClubRegistrationQuery.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
 using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
 
 
 namespace _07LabExer_Event
@@ -27,11 +28,11 @@ namespace _07LabExer_Event
         public string _Gender;
         public string _Program;
 
-        public int _Age;    
+        public int _Age;
 
         public ClubRegistrationQuery()
         {
-            
+
             this.connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\YourPath\\ClubDB.mdf\";Integrated Security=True;Connect Timeout=30";
 
             this.sqlConnect = new SqlConnection(this.connectionString);
@@ -39,6 +40,22 @@ namespace _07LabExer_Event
             this.dataTable = new DataTable();
             this.bindingSource = new BindingSource();
         }
+
+        public bool DisplayList()
+        {
+            { 
+                string ViewClubMembers = "SELECT StudentId, FirstName, MiddleName, LastName, Age, Gender, Program FROM ClubMembers";
+
+                this.sqlAdapter = new SqlDataAdapter(ViewClubMembers, this.sqlConnect);
+                
+                this.dataTable.Clear();    
+                this.sqlAdapter.Fill(this.dataTable);
+                this.bindingSource.DataSource = this.dataTable;
+
+                return true;
+            }
+        }
+
     }
 }
 
